79310b1c05d54f6f7ff344f962278a1622a5281d
0JeffZhang0 <1316217172@qq.com>
2018-02-20 21:53:05 +0800

    this is a new version

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime
-import tkinter as tk
-from tkinter import ttk
+from tkinter import *
+from tkinter.ttk import Combobox
+
 import calendar
 
 action_date = {}
@@ -9,7 +10,7 @@ action_date = {}
 def draw_days():
     column = 0
     for day in ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']:
-        ttk.Label(win, text=day, width=13).grid(column=column, row=2)
+        Label(win, text=day, width=13).grid(column=column, row=2)
         column += 1
 
 
@@ -20,8 +21,13 @@ def draw_dates():
     column = calendar.get_start_day(year.get(), month.get())
     if column >= 7:
         column -= 7
+    now = datetime.datetime.now()
     for date in range(1, calendar.get_num_of_days_in_month(year.get(), month.get()) + 1):
-        ttk.Label(body_frame, text=date, width=13, borderwidth=1, relief='solid').grid(column=column, row=row)
+        if year.get() == now.year and month.get() == now.month and date == now.day:
+            text_color = 'red'
+        else:
+            text_color = 'black'
+        Label(body_frame, text=date, width=13, borderwidth=1, relief='solid', fg=text_color).grid(column=column, row=row)
         column += 1
         if column == 7:
             column = 0
@@ -37,10 +43,6 @@ def go_to_today():
     action_date['day'] = now.day
 
 
-win = tk.Tk()
-win.title("Python GUI")
-
-
 def click_today():
     go_to_today()
     draw_dates()
@@ -50,21 +52,22 @@ def month_change(event):
     draw_dates()
 
 
-today_button = ttk.Button(win, text="Today",
-                          command=click_today)
+win = Tk()
+win.title("Python calendar")
+today_button = Button(win, text="Today", command=click_today)
 today_button.grid(column=2, row=1)
 
-year = tk.IntVar()
-year_edit = ttk.Entry(win, width=12, textvariable=year)
+year = IntVar()
+year_edit = Entry(win, width=12, textvariable=year)
 year_edit.bind("<Return>", month_change)
 year_edit.grid(column=0, row=1)
 year_edit.focus()
 
-body_frame = tk.Frame(win)
+body_frame = Frame(win)
 body_frame.grid(row=3, column=0, columnspan=7)
 
-month = tk.IntVar()
-month_list = ttk.Combobox(win, width=12, textvariable=month)
+month = IntVar()
+month_list = Combobox(win, width=12, textvariable=month)
 month_list.bind("<<ComboboxSelected>>", month_change)
 month_list['values'] = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
 month_list.grid(column=1, row=1)
